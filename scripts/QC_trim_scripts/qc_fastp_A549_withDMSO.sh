62b97d56db3832b3d3e7b966f9fcef3152819a3a
alysl56 <alysl56@nottingham.ac.uk>
2025-08-22 16:43:28 +0100

    Update qc_fastp_A549_withDMSO.sh

--- scripts/QC_trim_scripts/qc_fastp_A549_withDMSO.sh
+++ scripts/QC_trim_scripts/qc_fastp_A549_withDMSO.sh
@@ -1,1 +1,35 @@
+#!/bin/bash
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8g
+#SBATCH --time=48:00:00
+#SBATCH --job-name=qc_fastp_A549_withDMSO
+#SBATCH --output=/gpfs01/home/alysl56/projects/DMSO_A549_RNAseq/qc_reports/with_DMSO/slurm-%x-%j.out
+#SBATCH --error=/gpfs01/home/alysl56/projects/DMSO_A549_RNAseq/qc_reports/with_DMSO/slurm-%x-%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=alysl56@exmail.nottingham.ac.uk
 
+source $HOME/.bash_profile
+conda activate rna_seq_env
+
+RAW_DIR=/gpfs01/home/alysl56/projects/DMSO_A549_RNAseq/raw_data/with_DMSO
+TRIM_DIR=/gpfs01/home/alysl56/projects/DMSO_A549_RNAseq/raw_data/trimmed_data/with_DMSO
+QC_DIR=/gpfs01/home/alysl56/projects/DMSO_A549_RNAseq/qc_reports/with_DMSO
+
+mkdir -p "$TRIM_DIR" "$QC_DIR"
+cd "$RAW_DIR"
+
+for fq1 in *_1.fastq.gz; do
+    fq2=${fq1/_1.fastq.gz/_2.fastq.gz}
+    base=${fq1%_1.fastq.gz}
+    fastp -i "$fq1" -I "$fq2" \
+          -o "${TRIM_DIR}/${base}_1.trimmed.fastq.gz" \
+          -O "${TRIM_DIR}/${base}_2.trimmed.fastq.gz" \
+          -q 20 -u 30 -n 5 -l 50 -w 1 \
+          -h "${QC_DIR}/${base}.html" \
+          -j "${QC_DIR}/${base}.json"
+done
+
+conda deactivate
