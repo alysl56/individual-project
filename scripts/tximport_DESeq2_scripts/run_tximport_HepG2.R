cbd21f24d4694016f35f6c160ad9fcd76f4b5592
alysl56 <alysl56@nottingham.ac.uk>
2025-08-23 21:01:47 +0100

    Update run_tximport_HepG2.R

--- scripts/tximport_DESeq2_scripts/run_tximport_HepG2.R
+++ scripts/tximport_DESeq2_scripts/run_tximport_HepG2.R
@@ -1,1 +1,20 @@
+library(tximport)
+library(readr)
 
+base <- "/gpfs01/home/alysl56/projects/DMSO_HepG2_RNAseq"
+
+samples <- read.csv(file.path(base, "samplesheet.csv"), colClasses = c("character","character","character"))
+samples$quant_dir <- trimws(samples$quant_dir)
+
+tx2gene <- read.csv("/gpfs01/home/alysl56/references/tx2gene.csv", colClasses = c("character","character"))
+
+files <- file.path(samples$quant_dir, "quant.sf")
+names(files) <- samples$sample
+stopifnot(all(file.exists(files)))
+
+txi <- tximport(files, type = "salmon", tx2gene = tx2gene, ignoreTxVersion = TRUE)
+
+dir.create(file.path(base, "DE_analysis"), showWarnings = FALSE, recursive = TRUE)
+write.csv(txi$counts,    file.path(base, "DE_analysis", "gene_counts.csv"))
+write.csv(txi$abundance, file.path(base, "DE_analysis", "gene_tpm.csv"))
+saveRDS(txi,             file.path(base, "DE_analysis", "txi_counts.rds"))
