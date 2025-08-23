3b6bb7ee5020b2e0a7d9c44c99322202f4bf5631
alysl56 <alysl56@nottingham.ac.uk>
2025-08-23 03:31:08 +0100

    Update quant_salmon_Calu3.sh

--- scripts/salmon_quant_scripts/quant_salmon_Calu3.sh
+++ scripts/salmon_quant_scripts/quant_salmon_Calu3.sh
@@ -6,8 +6,8 @@
 #SBATCH --mem=8g
 #SBATCH --time=48:00:00
 #SBATCH --job-name=quant_salmon_Calu3
-#SBATCH --output=/gpfs01/home/alysl56/projects/DMSO_Calu3_RNAseq/salmon_quant/slurm-%x-%j.out
-#SBATCH --error=/gpfs01/home/alysl56/projects/DMSO_Calu3_RNAseq/salmon_quant/slurm-%x-%j.err
+#SBATCH --output=/gpfs01/home/alysl56/projects/DMSO_Calu-3_RNAseq/salmon_quant/slurm-%x-%j.out
+#SBATCH --error=/gpfs01/home/alysl56/projects/DMSO_Calu-3_RNAseq/salmon_quant/slurm-%x-%j.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=alysl56@exmail.nottingham.ac.uk
 
@@ -15,8 +15,8 @@ source $HOME/.bash_profile
 conda activate salmon_env
 
 INDEX=/gpfs01/home/alysl56/references/gencode_v38_index
-TRIM_DIR=/gpfs01/home/alysl56/projects/DMSO_Calu3_RNAseq/raw_data/trimmed_data
-OUT_DIR=/gpfs01/home/alysl56/projects/DMSO_Calu3_RNAseq/salmon_quant
+TRIM_DIR=/gpfs01/home/alysl56/projects/DMSO_Calu-3_RNAseq/raw_data/trimmed_data
+OUT_DIR=/gpfs01/home/alysl56/projects/DMSO_Calu-3_RNAseq/salmon_quant
 
 mkdir -p "${OUT_DIR}/with_DMSO" "${OUT_DIR}/without_DMSO"
 
